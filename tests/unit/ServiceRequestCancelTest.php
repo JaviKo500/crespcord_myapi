ad0e45c8debb862a9dc07dc0320182080ecb0234
JaViKo500 <javikogutierrez64@gmail.com>
2026-08-25 15:24:45 -0500

    refactor(services): extract request query helpers

--- tests/unit/ServiceRequestCancelTest.php
+++ tests/unit/ServiceRequestCancelTest.php
@@ -11,6 +11,7 @@ require_once __DIR__ . '/../../includes/myapi.services_common.inc';
 require_once __DIR__ . '/../../includes/myapi.provider_role.inc';
 require_once __DIR__ . '/../../includes/myapi.building_admin.inc';
 require_once __DIR__ . '/../../includes/myapi.service_request_files.inc';
+require_once __DIR__ . '/../../includes/myapi.service_request_query.inc';
 require_once __DIR__ . '/../../resources/service_request.resource.inc';
 
 /**
